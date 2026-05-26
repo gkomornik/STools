4fdece2ddf8cef9a58b73bd8a40460f6938c0938
gkomornik <80854288+gkomornik@users.noreply.github.com>
2026-05-26 23:14:34 +0200

    Format output with tabs in HelloWorld.ps1

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,7 +1,7 @@
 Write-Host "Hello World!!"
-"Date: {0}" -f (Get-Date)
-"Computer name: {0}" -f (hostname)
-"User name: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).UserName
-"Operating System: {0}" -f (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
-"Model: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).Model
-"IP Address: {0}" -f (Get-NetIPAddress -InterfaceIndex (Get-NetConnectionProfile).InterfaceIndex | Where-Object AddressFamily -eq IPv4).IPAddress
+"Date`t`t: {0}" -f (Get-Date)
+"Computer name`t: {0}" -f (hostname)
+"User name`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).UserName
+"Operating System`t: {0}" -f (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
+"Model`t`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).Model
+"IP Address`t`t: {0}" -f (Get-NetIPAddress -InterfaceIndex (Get-NetConnectionProfile).InterfaceIndex | Where-Object AddressFamily -eq IPv4).IPAddress
