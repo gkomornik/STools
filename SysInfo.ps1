6b903481ea6c20770f2c40455cfaef1fdd965669
gkomornik <80854288+gkomornik@users.noreply.github.com>
2026-07-09 14:52:45 +0200

    Format memory usage output to two decimal places
    Updated memory usage output format to include two decimal places.

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -7,5 +7,5 @@ Write-Host "==================" -ForegroundColor Yellow
 "Operating System`t: {0}" -f (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
 "Model`t`t`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).Model
 "Processor`t`t: {0}" -f ((Get-CimInstance -ClassName Win32_Processor).Name)
-"Memory`t`t`t: {0} GB used of {1} GB" -f ([Math]::Round((Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2)-[Math]::Round((Get-CimInstance Win32_OperatingSystem).FreePhysicalMemory / 1MB, 2)),([Math]::Round((Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2))
+"Memory`t`t`t: {0:N2} GB used of {1:N2} GB" -f ([Math]::Round((Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2)-[Math]::Round((Get-CimInstance Win32_OperatingSystem).FreePhysicalMemory / 1MB, 2)),([Math]::Round((Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2))
 "IP Address`t`t: {0}" -f (Get-NetIPAddress -InterfaceIndex (Get-NetConnectionProfile).InterfaceIndex | Where-Object AddressFamily -eq IPv4).IPAddress
