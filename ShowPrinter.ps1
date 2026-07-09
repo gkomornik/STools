f7870c6c468c91cadfc5b99da8337c40e686bde7
gkomornik <80854288+gkomornik@users.noreply.github.com>
2026-07-09 14:19:05 +0200

    Fix date formatting in ShowPrinter.ps1

--- ShowPrinter.ps1
+++ ShowPrinter.ps1
@@ -1,3 +1,3 @@
 Write-Host "Show Printer"
-"Date`t`t`t: {0}" -f (Get-Date)
+"Date`t`t: {0}" -f (Get-Date)
 Get-Printer | Format-List @{Label="Name";Expression={$_.Name}},@{Label="DriverName";Expression={$_.DriverName}},@{Label="PortName";Expression={$_.PortName.SubString(0,10)}},@{Label="PrinterHostAddress";Expression={$portName=$_.PortName;(Get-PrinterPort | Where-Object {$_.Name -match $portName} | Select-Object PrinterHostAddress).PrinterHostAddress}}
