b8c66208d3593b246cdad4a698a3dc35835e009f
gkomornik <80854288+gkomornik@users.noreply.github.com>
2026-07-09 14:26:18 +0200

    Fix typo in SysInfo.ps1 for processor display

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -4,4 +4,5 @@ Write-Host "System information"
 "User name`t`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).UserName
 "Operating System`t: {0}" -f (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
 "Model`t`t`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).Model
+"Procesor't't't: {0}" -f ((Get-CimInstance -ClassName Win32_Processor).Name)
 "IP Address`t`t: {0}" -f (Get-NetIPAddress -InterfaceIndex (Get-NetConnectionProfile).InterfaceIndex | Where-Object AddressFamily -eq IPv4).IPAddress
