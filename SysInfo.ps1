cbeacc6cce95d4edaa679ed616b23138744f2dbf
gkomornik <80854288+gkomornik@users.noreply.github.com>
2026-07-09 14:39:12 +0200

    Fix typo in processor output line

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -4,5 +4,6 @@ Write-Host "System information"
 "User name`t`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).UserName
 "Operating System`t: {0}" -f (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
 "Model`t`t`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).Model
-"Procesor`t`t`t: {0}" -f ((Get-CimInstance -ClassName Win32_Processor).Name)
+"Procesor`t`t: {0}" -f ((Get-CimInstance -ClassName Win32_Processor).Name)
+"Memory`t`t`t: {0} used of {1}" -f ([Math]::Round($mem.TotalVisibleMemorySize / 1MB, 2)-[Math]::Round($mem.FreePhysicalMemory / 1MB, 2)),([Math]::Round($mem.TotalVisibleMemorySize / 1MB, 2))
 "IP Address`t`t: {0}" -f (Get-NetIPAddress -InterfaceIndex (Get-NetConnectionProfile).InterfaceIndex | Where-Object AddressFamily -eq IPv4).IPAddress
