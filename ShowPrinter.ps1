1461e6ce677a05b3192124013d0b50258d3da7f1
Grzegorz Komornik <gkomornik@windowslive.com>
2026-07-10 13:02:43 +0200

    Fixed Header info

--- ShowPrinter.ps1
+++ ShowPrinter.ps1
@@ -1,3 +1,4 @@
-Write-Host "Show Printer"
-"Date`t`t: {0}" -f (Get-Date)
+Write-Host "Show Printer" -ForegroundColor Yellow
+Write-Host "============" -ForegroundColor Yellow
+
 Get-Printer | Format-List @{Label="Name";Expression={$_.Name}},@{Label="DriverName";Expression={$_.DriverName}},@{Label="PortName";Expression={$_.PortName.SubString(0,10)}},@{Label="PrinterHostAddress";Expression={$portName=$_.PortName;(Get-PrinterPort | Where-Object {$_.Name -match $portName} | Select-Object PrinterHostAddress).PrinterHostAddress}}
