552ff1da0f17a8bd913436517df8afc6910078ac
Grzegorz Komornik <gkomornik@windowslive.com>
2026-07-10 13:41:53 +0200

    Added Last Bootup Time info

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -1,10 +1,11 @@
 Write-Host "System information" -ForegroundColor Yellow
 Write-Host "==================" -ForegroundColor Yellow
 ""
-"Date`t`t`t: {0}" -f (Get-Date)
+"Date`t`t`t: {0}" -f (Get-Date -Format 'dddd').ToUpper()[0]+(Get-Date -Format 'dddd, dd MMMM yyyy HH:mm:ss').Substring(1)
 "Computer name`t`t: {0}" -f (hostname)
 "User name`t`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).UserName
 "Operating System`t: {0} ({1})" -f (Get-CimInstance -ClassName Win32_OperatingSystem).Caption,(Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").DisplayVersion
+"Last bootup time`t: {0}{1}, {2:dd} {2:MMMM} {2:yyy} {2:hh}:{2:mm}:{2:ss} ({3} d {4})" -f (Get-CimInstance -ClassName win32_operatingSystem).LastBootUpTime.ToString("dddd").ToUpper()[0],(Get-CimInstance -ClassName win32_operatingSystem).LastBootUpTime.ToString("dddd").Substring(1),(Get-CimInstance -ClassName win32_operatingSystem).LastBootUpTime,((Get-Date)-(Get-CimInstance -ClassName win32_operatingSystem).LastBootUpTime).ToString().Split(".")[0],((Get-Date)-(Get-CimInstance -ClassName win32_operatingSystem).LastBootUpTime).ToString().Split(".")[1]
 "Model`t`t`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).Model
 "Processor`t`t: {0} ({1} Cores)" -f ((Get-CimInstance -ClassName Win32_Processor).Name),(Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
 "Memory`t`t`t: {0:N2} GB used of {1:N2} GB" -f ([Math]::Round((Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2)-[Math]::Round((Get-CimInstance Win32_OperatingSystem).FreePhysicalMemory / 1MB, 2)),([Math]::Round((Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2))
