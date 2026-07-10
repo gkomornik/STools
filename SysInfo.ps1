c6cc3f374dd2eb052979ba70dc4eae0d70fe545f
Grzegorz Komornik <gkomornik@windowslive.com>
2026-07-10 11:59:52 +0200

    Added CPU Cpores

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -4,7 +4,7 @@ Write-Host "==================" -ForegroundColor Yellow
 "Computer name`t`t: {0}" -f (hostname)
 "User name`t`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).UserName
 "Operating System`t: {0}" -f (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
-"Model`t`t`t: {0} ({1} Cores)" -f (Get-CimInstance -ClassName Win32_ComputerSystem).Model,(Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
-"Processor`t`t: {0}" -f ((Get-CimInstance -ClassName Win32_Processor).Name)
+"Model`t`t`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).Model
+"Processor`t`t: {0} ({1} Cores)" -f ((Get-CimInstance -ClassName Win32_Processor).Name),(Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
 "Memory`t`t`t: {0:N2} GB used of {1:N2} GB" -f ([Math]::Round((Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2)-[Math]::Round((Get-CimInstance Win32_OperatingSystem).FreePhysicalMemory / 1MB, 2)),([Math]::Round((Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2))
 "IP Address`t`t: {0}" -f (Get-NetIPAddress -InterfaceIndex (Get-NetConnectionProfile).InterfaceIndex | Where-Object AddressFamily -eq IPv4).IPAddress
