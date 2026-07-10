d499901b6414dc65e5cca122984a6715175160e9
Grzegorz Komornik <gkomornik@windowslive.com>
2026-07-10 12:26:04 +0200

    Added OS Build Version

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -3,7 +3,7 @@ Write-Host "==================" -ForegroundColor Yellow
 "Date`t`t`t: {0}" -f (Get-Date)
 "Computer name`t`t: {0}" -f (hostname)
 "User name`t`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).UserName
-"Operating System`t: {0}" -f (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
+"Operating System`t: {0} ({1})" -f (Get-CimInstance -ClassName Win32_OperatingSystem).Caption,(Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").DisplayVersion
 "Model`t`t`t: {0}" -f (Get-CimInstance -ClassName Win32_ComputerSystem).Model
 "Processor`t`t: {0} ({1} Cores)" -f ((Get-CimInstance -ClassName Win32_Processor).Name),(Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
 "Memory`t`t`t: {0:N2} GB used of {1:N2} GB" -f ([Math]::Round((Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2)-[Math]::Round((Get-CimInstance Win32_OperatingSystem).FreePhysicalMemory / 1MB, 2)),([Math]::Round((Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2))
