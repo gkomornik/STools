80acdce5fc086bf71dcf05fc5349a3ff3fb63e2e
Grzegorz Komornik <gkomornik@windowslive.com>
2026-07-13 14:16:44 +0200

    Fixed

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -12,16 +12,16 @@ Write-Host "--------------------------------------------------------------------
 Write-Host "COMPUTER:" -ForegroundColor Cyan
 "  {0,-20}: {1}" -f "Model",(Get-CimInstance -ClassName Win32_ComputerSystem).Model
 
-$chassisType=""
+$chassisType = ""
 switch ((Get-CimInstance -ClassName Win32_SystemEnclosure).ChassisTypes) {
-    3 { $chassisType='Desktop' }
-    8 { $chassisType='Portable'}
-    9 { $chassisType='Laptop'}
-    10 { $chassisType='Notebook'}
-    13 { $chassisType='All in One'}
-    14 { $chassisType='Sub Notebook'}
-    30 { $chassisType='Tablet'}
-    Default { $chassisType='none' }
+    3 { $chassisType = 'Desktop' }
+    8 { $chassisType = 'Portable'}
+    9 { $chassisType = 'Laptop'}
+    10 { $chassisType = 'Notebook'}
+    13 { $chassisType = 'All in One'}
+    14 { $chassisType = 'Sub Notebook'}
+    30 { $chassisType = 'Tablet'}
+    Default { $chassisType = 'none' }
 }
 "  {0,-20}: {1}" -f "Chassis type",$chassisType
 "  {0,-20}: {1}" -f "Serial Number",(Get-CimInstance -ClassName Win32_SystemEnclosure).SerialNumber
@@ -31,7 +31,7 @@ switch ((Get-CimInstance -ClassName Win32_SystemEnclosure).ChassisTypes) {
 Write-Host "OPERATING SYSTEM:" -ForegroundColor Cyan
 #"User name`t`t: {0}" -f (whoami)
 #"User name`t`t: " -f (whoami) | Write-Host -ForegroundColor Green -NoNewline;"{0}" -f (whoami) | Write-Host
-$os=(Get-CimInstance -ClassName Win32_OperatingSystem)
+$os = Get-CimInstance -ClassName Win32_OperatingSystem
 "  {0,-20}: {1} {2} ({3})" -f "Name",$os.Caption,$os.OSArchitecture,(Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").DisplayVersion
 #"Operating System`t: {0} {1} ({2})" -f (Get-CimInstance -ClassName Win32_OperatingSystem).Caption,(Get-CimInstance -ClassName Win32_OperatingSystem).OSArchitecture,(Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").DisplayVersion
 "  {0,-20}: {1}{2}, {3:dd} {3:MMMM} {3:yyy} {3:hh}:{3:mm}:{3:ss} ({4}d {5})" -f "Last bootup time",$os.LastBootUpTime.ToString("dddd").ToUpper()[0],$os.LastBootUpTime.ToString("dddd").Substring(1),$os.LastBootUpTime,((Get-Date)-$os.LastBootUpTime).ToString().Split(".")[0],((Get-Date)-$os.LastBootUpTime).ToString().Split(".")[1]
@@ -40,7 +40,7 @@ $os=(Get-CimInstance -ClassName Win32_OperatingSystem)
 
 # processor info
 Write-Host "PROCESSOR:" -ForegroundColor Cyan
-$processor=Get-CimInstance -ClassName Win32_Processor
+$processor = Get-CimInstance -ClassName Win32_Processor
 #"Processor`t`t: {0} ({1} Cores)" -f ((Get-CimInstance -ClassName Win32_Processor).Name),(Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
 "  {0,-20}: {1} ({2} Cores)" -f "Model",$processor.Name,$processor.NumberOfLogicalProcessors
 ""
@@ -59,8 +59,8 @@ Get-CimInstance -ClassName Win32_VideoController | ForEach-Object {
 # memory info
 Write-Host "MEMORY:" -ForegroundColor Cyan
 $mem = Get-CimInstance Win32_OperatingSystem
-$totalRam = [Math]::Round($mem.TotalVisibleMemorySize / 1MB, 2) # Wynik w GB
-$freeRam = [Math]::Round($mem.FreePhysicalMemory / 1MB, 2)   # Wynik w GB
+$totalRam = [Math]::Round($mem.TotalVisibleMemorySize / 1MB, 2)
+$freeRam = [Math]::Round($mem.FreePhysicalMemory / 1MB, 2)
 $usedRam = $totalRam - $freeRam
 #"Memory`t`t`t: {0:N2} GB used of {1:N2} GB" -f ([Math]::Round((Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2)-[Math]::Round((Get-CimInstance Win32_OperatingSystem).FreePhysicalMemory / 1MB, 2)),([Math]::Round((Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2))
 "  {0,-20}: {1:N2} GB used of {2:N2} GB" -f "RAM",$usedRam,$totalRam
@@ -72,9 +72,9 @@ $pfUsed = if($pageFile) { ($pageFile | Measure-Object -Property CurrentUsage -Su
 
 # disc info
 Write-Host "DISC:" -ForegroundColor Cyan
-$sysDrive=(Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$Env:SystemDrive'").DeviceID
+#$sysDrive = (Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$Env:SystemDrive'").DeviceID
 Get-CimInstance -ClassName Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3} | ForEach-Object {
-    if ($_.DeviceID -eq $Env:SystemDrive) {$sysDrive="*Sys"} else {$sysDrive=""}
+    if ($_.DeviceID -eq $Env:SystemDrive) { $sysDrive = "*SYS" } else {$sysDrive = "" }
     "  {0,-20}: Size: {1,8:N2} GB   Free space: {2,8:N2} GB ({3} %) {4}" -f ($_.VolumeName+" ("+$_.DeviceID+")"),($_.Size / 1GB),($_.FreeSpace / 1GB),[Math]::Round(  (($_.FreeSpace/$_.Size)*100) , 1),$sysDrive
 }
 #"System drive`t`t: {0} Size: {1:N2} GB | Free space: {2:N2} GB ({3} %)" -f (Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$Env:SystemDrive'").DeviceID,((Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$Env:SystemDrive'").Size / 1GB),((Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$Env:SystemDrive'").FreeSpace / 1GB),[Math]::Round((((Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$Env:SystemDrive'").FreeSpace) / ((Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$Env:SystemDrive'").Size)) * 100, 1)
