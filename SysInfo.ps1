58a6280336e10a9e7408cd7081f0bc0021e68f9d
Grzegorz Komornik <gkomornik@windowslive.com>
2026-07-13 13:48:16 +0200

    Fixed

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -46,7 +46,7 @@ Write-Host "PROCESSOR:" -ForegroundColor Cyan
 # video card info
 Write-Host "VIDEO CARD:" -ForegroundColor Cyan
 Get-CimInstance -ClassName Win32_VideoController | ForEach-Object {
-    "  {0,-20}: {1,-30} VRAM: {2} GB" -f "Model",$_.Name,[Math]::Round( ($_.AdapterRAM / 1GB), 2)
+    "  {0,-20}: {1,-40} VRAM: {2} GB" -f "Model",$_.Name,[Math]::Round( ($_.AdapterRAM / 1GB), 2)
 }
 #"Graphic card`t`t: {0} | VRAM: {1} GB" -f (Get-CimInstance Win32_VideoController).Name,([Math]::Round((Get-CimInstance Win32_VideoController).AdapterRam / 1GB, 2))
 #"Graphic card`t`t: {0}" -f (Get-CimInstance Win32_VideoController).Name
