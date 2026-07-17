61c34120e4df64640bfd940d12617b41917149e7
Grzegorz Komornik <gkomornik@windowslive.com>
2026-07-17 12:17:29 +0200

    Fixed

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -60,7 +60,7 @@ Write-Host "OPERATING SYSTEM:" -ForegroundColor Cyan
 #"User name`t`t: " -f (whoami) | Write-Host -ForegroundColor Green -NoNewline;"{0}" -f (whoami) | Write-Host
 $os_version = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
 #"  {0,-16}: {1} {2} ({3})" -f "Name",$os.Caption,$os.OSArchitecture,$os_version.DisplayVersion
-if ($os_version.DisplayVersion -ne "") {
+if ($os_version.DisplayVersion -eq "") {
     "  {0,-16}: {1} {2} ({3})" -f "Name",$os.Caption,$os.OSArchitecture,$os_version.DisplayVersion
 } else {
     "  {0,-16}: {1} {2}" -f "Name",$os.Caption,$os.OSArchitecture
