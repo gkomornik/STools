99e85378250dd337222f74227f31edbd303599b2
Grzegorz Komornik <gkomornik@windowslive.com>
2026-08-04 10:25:31 +0200

    Added Swap Peak

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -187,7 +187,8 @@ $usedRam = $totalRam - $freeRam
 $pageFile = Get-CimInstance Win32_PageFileUsage
 $pfTotal = if($pageFile) { ($pageFile | Measure-Object -Property AllocatedBaseSize -Sum).Sum } else { 0 }
 $pfUsed = if($pageFile) { ($pageFile | Measure-Object -Property CurrentUsage -Sum).Sum } else { 0 }
-"  {0,-16}: {1,5} MB used of {2,5} MB ({3} %)" -f "Swap file",$pfUsed,$pfTotal,[Math]::Round(($pfUsed/$pfTotal)*100,1)
+$pfPeak= if($pageFile) { ($pageFile | Measure-Object -Property PeakUsage -Sum).Sum } else { 0 }
+"  {0,-16}: {1,5} MB used of {2,5} MB ({3} %) {4,5}: {5} MB" -f "Swap file",$pfUsed,$pfTotal,[Math]::Round(($pfUsed/$pfTotal)*100,1),"Peak",$pfPeak
 ""
 
 # disk drive
