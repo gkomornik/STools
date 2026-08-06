1f3339b3141d0ee2e853c785faa365979df53007
Grzegorz Komornik <gkomornik@windowslive.com>
2026-08-06 13:50:38 +0200

    Fixed

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -290,10 +290,12 @@ Get-CimInstance -ClassName Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3}
     }
     #>
     "  {0,-16}: FileSystem: {1,-6} Size: {2,7:N2} GB   Free: {3,7:N2} GB ({4} %)  {5,5}" -f ($_.DeviceID+" ("+$_.VolumeName+")"),$_.FileSystem,($_.Size / 1GB),($_.FreeSpace / 1GB),[Math]::Round(  (($_.FreeSpace/$_.Size)*100) , 1),$sysDrive
-    "  {0,-16}  BitLocker ProtectionStatus     : {1}" -f "",((Get-BitLockerVolume -MountPoint $_.DeviceID).ProtectionStatus)
-    "  {0,-16}  BitLocker VolumeStatus         : {1}" -f "",((Get-BitLockerVolume -MountPoint $_.DeviceID).VolumeStatus)
-    "  {0,-16}  BitLocker EncryptionPercentage : {1}" -f "",((Get-BitLockerVolume -MountPoint $_.DeviceID).EncryptionPercentage)
-    "  {0,-16}  BitLocker EncryptionMethod     : {1}" -f "",((Get-BitLockerVolume -MountPoint $_.DeviceID).EncryptionMethod)
+    if ($isAdmin -and $isBitLockerCMDSupport) {
+        "  {0,-16}  BitLocker ProtectionStatus     : {1}" -f "",((Get-BitLockerVolume -MountPoint $_.DeviceID).ProtectionStatus)
+        "  {0,-16}  BitLocker VolumeStatus         : {1}" -f "",((Get-BitLockerVolume -MountPoint $_.DeviceID).VolumeStatus)
+        "  {0,-16}  BitLocker EncryptionPercentage : {1}" -f "",((Get-BitLockerVolume -MountPoint $_.DeviceID).EncryptionPercentage)
+        "  {0,-16}  BitLocker EncryptionMethod     : {1}" -f "",((Get-BitLockerVolume -MountPoint $_.DeviceID).EncryptionMethod)
+    }
 }
 if (!$isAdmin -and $isBitLockerCMDSupport) {Write-Host "  * Run with elevated privileges to see information about Bitlocker" -ForegroundColor DarkYellow}
 #"System drive`t`t: {0} Size: {1:N2} GB | Free space: {2:N2} GB ({3} %)" -f (Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$Env:SystemDrive'").DeviceID,((Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$Env:SystemDrive'").Size / 1GB),((Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$Env:SystemDrive'").FreeSpace / 1GB),[Math]::Round((((Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$Env:SystemDrive'").FreeSpace) / ((Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID='$Env:SystemDrive'").Size)) * 100, 1)
