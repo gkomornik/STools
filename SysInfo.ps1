9b75b1e8faf07f485e017fef18f7626761d6221b
Grzegorz Komornik <gkomornik@windowslive.com>
2026-08-11 09:24:23 +0200

    Added Bios info

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -161,6 +161,16 @@ Get-CimInstance -ClassName Win32_BaseBoard | ForEach-Object {
     "  {0,-16}: {1}" -f "Model",$_.Product
     "  {0,-16}: {1}" -f "Manufacturer",$_.Manufacturer
     "  {0,-16}: {1}" -f "Version",$_.Version
+    ""
+}
+
+# base board info 
+Write-Host "BIOS:" -ForegroundColor Cyan
+Get-CimInstance -ClassName Win32_Bios | ForEach-Object {
+    "  {0,-16}: {1}" -f "Manufacturer",$_.Manufacturer
+    "  {0,-16}: {1}" -f "SMBIOSBIOSVer",$_.SMBIOSBIOSVersion
+    "  {0,-16}: {1}" -f "Version",$_.Version
+    #"  {0,-16}: {1}" -f "Version",$_.Version
 
     ""
 }
@@ -224,6 +234,7 @@ Get-CimInstance -ClassName Win32_PhysicalMemory | ForEach-Object {
     "  {0,-16}  SerialNumber        : {1}" -f "",$_.SerialNumber
     "  {0,-16}  Capacity            : {1} GB" -f "",($_.Capacity / 1GB)
     "  {0,-16}  DeviceLocator       : {1}" -f "",$_.DeviceLocator
+    "  {0,-16}  Speed               : {1} MHz" -f "",$_.Speed
     ""
 }
 "  {0,-16}: {1} GB" -f "Total size",((get-CimInstance -ClassName win32_physicalmemory | Measure-Object -Property Capacity -Sum).Sum /1gb)
