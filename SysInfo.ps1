bf228289b03b2b7dc2e5b1e9164b9ac1a451c6d6
Grzegorz Komornik <gkomornik@windowslive.com>
2026-08-11 11:58:08 +0200

    Fixed

--- SysInfo.ps1
+++ SysInfo.ps1
@@ -322,7 +322,8 @@ Get-CimInstance -ClassName Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3}
     } else {
         "  {0,-16}: FileSystem             : {1,-6}" -f $_.DeviceID,$_.FileSystem
     }
-    "  {0,-16}  Size                   : {1:N2} GB" -f "",($_.Size / 1GB)
+    #"  {0,-16}  Size                   : {1:N2} GB" -f "",($_.Size / 1GB)
+    if (($_.Size / 1TB) -ge 1) {"  {0,-16}  Size                   : {1:N2} TB" -f "",($_.Size / 1TB)} else {"  {0,-16}  Size                   : {1:N2} GB" -f "",($_.Size / 1GB)}
     "  {0,-16}  Free                   : {1:N2} GB ({2} %)" -f "",($_.FreeSpace / 1GB),[Math]::Round(  (($_.FreeSpace/$_.Size)*100) , 1)
 
     if ($sysDrive -eq "[SYSTEM DRIVE]") {
